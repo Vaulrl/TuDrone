96929a02796bc62a176099c8e30e1bbacbb0e6eb
JZJ <1029594694@qq.com>
2020-08-05 21:14:50 +0800

    飞行器完成

---
+++ Source/TuDrone/TuDroneGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TuDroneGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TUDRONE_API ATuDroneGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
